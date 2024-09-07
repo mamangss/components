2109e18c29f5a5a1f9e96f16011cb0fe14ca5d10
Joshua Mamangun <mamangunjoshua@gmail.com>
2024-09-07 14:03:15 +0800

    Fix follow cursor on hover

--- src/components/FollowCursorOnHover/FollowCursorOnHover.tsx
+++ src/components/FollowCursorOnHover/FollowCursorOnHover.tsx
@@ -6,52 +6,72 @@ const FollowCursorOnHover: React.FC<IFollowCursorOnHover> = ({
 	list = [],
 }) => {
 
-	const [widthList, setWidthList] = useState<number[]>([0, 0, 0, 0]);
-	const [directionList, setDirectionList] = useState<number[]>([-1, -1, -1, -1]); // 1 - to right, -1 to left
 	const [lastIndex, setLastIndex] = useState<number>(-1);
-
-	const handleMouseMove = (e: any, index: number) => {
-		e.stopPropagation();
-
-		var tempWidthList = [...widthList];
-		var tempDirectionList = [...directionList];
-
-		// Item's width = cursor position - offset position of the item
-		tempWidthList[index] = (e.clientX || 0) - (e.target.offsetParent.offsetLeft || 0);
-
-		// If the hovered item changed to another item
-		if (lastIndex !== -1 && lastIndex !== index) {
-
-			// Setthe previous hovered item's width to zero
-			tempWidthList[lastIndex] = 0;
-
-			// Set the item's bg direction based on last index
-			tempDirectionList[lastIndex] = lastIndex < index ? 1 : -1;
-			tempDirectionList[index] = lastIndex < index ? -1 : 1;
-			setDirectionList(tempDirectionList);
-		}
-
-		setWidthList(tempWidthList);
-		setLastIndex(index);
-	};
-
-	const resetLastIndex = () => {
-		var tempWidthList = [...widthList];
-		tempWidthList[lastIndex] = 0;
-		setDirectionList(list.map(() => -1));
-		setWidthList(tempWidthList);
-		setLastIndex(-1);
-	};
+	const [startPointList, setStartPointList] = useState<number[]>([-1, -1, -1, -1]); // -1 left, 1 right
+	const [widthList, setWidthList] = useState<number[]>([0, 0, 0, 0]);
 
 	useEffect(() => {
 		setWidthList(list.map(() => 0));
-		setDirectionList(list.map(() => -1));
+		setStartPointList(list.map(() => -1));
 	}, [list]);
+	
+	// Handlers Start
+	const handleMouseHover = (e: any, index: number) => {
+		e.stopPropagation();
+		
+		var tempStartPointList = [...startPointList];
+		var tempWidthList = [...widthList];
+
+		// Set start points
+		// If the first hovered item is the last item, then set the item's start point to right (1)
+		if (lastIndex === -1 && index === list.length - 1) {
+			tempStartPointList[index] = 1;
+		
+		// If the there's a lastIndex, set the start point based from the lastIndex
+		} else if (lastIndex !== -1) {
+			tempStartPointList[index] = index > lastIndex ? -1 : 1;
+			tempStartPointList[lastIndex] = tempStartPointList[index] * -1; // Set the start point as the opposite to look smooth
+
+		} else if (lastIndex === index) {
+			tempStartPointList[index] = -1;
+		}
+
+		// Set widths
+		tempWidthList[index] = tempStartPointList[index] === -1 ? 
+			(e.clientX || 0) - (e.target.offsetParent.offsetLeft || 0) // For left starting point
+			: (e.target.clientWidth || 0) - ((e.clientX || 0) - (e.target.offsetParent.offsetLeft || 0)); // For right starting point
+		
+		// Set the last item's width to 0
+		if (lastIndex !== index) {
+			tempWidthList[lastIndex] = 0;
+		}
+
+		setStartPointList(tempStartPointList);
+		setWidthList(tempWidthList);
+	};
+
+	const handleMouseLeave = (e: any, index: number) => {
+
+		var tempWidthList = [...widthList];
+		tempWidthList[index] = 0;
+		
+		setWidthList(tempWidthList);
+		setLastIndex(index);
+	}
+
+	const handleMouseClick = (e: any, index: number) => {
+		e.stopPropagation();
+
+		var tempWidthList = [...widthList];
+		tempWidthList[index] = e.target.clientWidth || 0;
+
+		setWidthList(tempWidthList);
+	};
+	// Handlers End
 
 	return (
 		<div 
 			className={styles.list}
-			onMouseLeave={resetLastIndex}
 		>
 			{list.map((item, key) => (
 				<div
@@ -64,15 +84,17 @@ const FollowCursorOnHover: React.FC<IFollowCursorOnHover> = ({
 
 					<div 
 						className={styles.bgSensor}
-						onMouseMove={(e: any) => handleMouseMove(e, key)}
+						onMouseMove={(e: any) => handleMouseHover(e, key)}
+						onMouseLeave={(e: any) => handleMouseLeave(e, key)}
+						onMouseDown={(e: any) => handleMouseClick(e, key)}
 					></div>
 
 					<div 
 						className={styles.bg}
 						style={{ 
 							width: widthList[key],
-							right: directionList[key] == 1 ? '0px' : 'none',
-							left: directionList[key] == -1 ? '0px' : 'auto'
+							left: startPointList[key] === -1 ? '0px' : 'auto',
+							right: startPointList[key] === 1 ? '0px' : 'none',
 						}}
 					></div>
 				</div>
